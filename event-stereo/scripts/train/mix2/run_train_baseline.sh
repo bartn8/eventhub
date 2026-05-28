8b91d79a626dc871b38b9043b2c9a332a581de5d
Luca Bartolomei <bartn8@hotmail.it>
2026-05-28 09:05:01 +0200

    add scripts + fix configs

---
+++ event-stereo/scripts/train/mix2/run_train_baseline.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DATA_ROOT_VAL="event-stereo/datasets/dsec"
+# compute codebase path from this script location (one level up to event-stereo)
+SCRIPT_DIR="$( dirname "$( realpath "$0" )" )"
+CODEBASE_PATH="$( realpath "$SCRIPT_DIR/../.." )"
+CONDA_PATH="/leonardo_scratch/fast/EUHPC_D35_218/miniconda3/bin/conda"
+CONDA_ENV=eventhub
+ER="tencode"
+
+cd $CODEBASE_PATH
+eval "$( $CONDA_PATH shell.bash hook)"
+conda activate $CONDA_ENV
+
+RESUME=""
+# RESUME="--resume"
+
+SEED="--seed 42"
+
+DATALOADER_WORKERS="--num_workers 8"
+
+PRETRAIN=""
+# PRETRAIN="--pretrain_path ..."
+
+echo "CUDA_VISIBLE_DEVICES=0 python src/main.py --data_root_validation $DATA_ROOT_VAL --validate --save_root trainings/mix2/${ER}/baseline \
+  --config_path configs/train/baseline/mix2/config_${ER}.yaml $SEED $RESUME --seq_size 0 $DATALOADER_WORKERS $PRETRAIN"
+
+CUDA_VISIBLE_DEVICES=0 python src/main.py --data_root_validation $DATA_ROOT_VAL --validate --save_root trainings/mix2/${ER}/baseline \
+  --config_path configs/train/baseline/mix2/config_${ER}.yaml $SEED $RESUME --seq_size 0 $DATALOADER_WORKERS $PRETRAIN
---
+++ event-stereo/scripts/train/mix2/run_train_stereoanywhere.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DATA_ROOT_VAL="event-stereo/datasets/dsec"
+# compute codebase path from this script location (one level up to event-stereo)
+SCRIPT_DIR="$( dirname "$( realpath "$0" )" )"
+CODEBASE_PATH="$( realpath "$SCRIPT_DIR/../.." )"
+CONDA_PATH="/leonardo_scratch/fast/EUHPC_D35_218/miniconda3/bin/conda"
+CONDA_ENV=eventhub
+ER="tencode"
+
+cd $CODEBASE_PATH
+eval "$( $CONDA_PATH shell.bash hook)"
+conda activate $CONDA_ENV
+
+RESUME=""
+# RESUME="--resume"
+
+SEED="--seed 42"
+
+DATALOADER_WORKERS="--num_workers 8"
+
+PRETRAIN="--pretrain_path $CODEBASE_PATH/weights/stereoanywhere_vda_vits.pth"
+
+echo "CUDA_VISIBLE_DEVICES=0 python src/main.py --data_root_validation $DATA_ROOT_VAL --validate --save_root trainings/mix2/${ER}/stereoanywhere \
+  --config_path configs/train/stereoanywhere/mix2/config_${ER}.yaml $SEED $RESUME $DATALOADER_WORKERS $PRETRAIN"
+
+CUDA_VISIBLE_DEVICES=0 python src/main.py --data_root_validation $DATA_ROOT_VAL --validate --save_root trainings/mix2/${ER}/stereoanywhere \
+  --config_path configs/train/stereoanywhere/mix2/config_${ER}.yaml $SEED $RESUME $DATALOADER_WORKERS $PRETRAIN
